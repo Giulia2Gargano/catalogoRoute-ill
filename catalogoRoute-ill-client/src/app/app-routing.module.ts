be8d8499c8a55ae4ca3835ba5da56d0ccada7516
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-02-25 15:49:45 +0100

    Update Path

--- catalogoRoute-ill-client/src/app/app-routing.module.ts
+++ catalogoRoute-ill-client/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CercaComponent } from './cerca/cerca.component';
+import { ContaComponent } from './conta/conta.component';
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   { path: 'main-page', component: MainComponent },
-  { path: 'search-page', component: MainComponent },
-  { path: 'count-page', component: MainComponent },
+  { path: 'search-page', component: CercaComponent },
+  { path: 'count-page', component: ContaComponent },
   { path: '', redirectTo: '/main-page', pathMatch: 'full' }
 ];
 
