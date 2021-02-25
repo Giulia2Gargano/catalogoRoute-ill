9aab80760d708c44b60036eb9b557456531796ae
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-02-25 14:57:01 +0100

    Update Mediator

--- catalogoRoute-ill-client/src/app/cerca/cerca.component.ts
+++ catalogoRoute-ill-client/src/app/cerca/cerca.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MediatorService } from '../mediator.service';
 
 @Component({
   selector: 'app-cerca',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CercaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public med:MediatorService) { }
 
   ngOnInit(): void {
   }
--- catalogoRoute-ill-client/src/app/conta/conta.component.ts
+++ catalogoRoute-ill-client/src/app/conta/conta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MediatorService } from '../mediator.service';
 
 @Component({
   selector: 'app-conta',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public med:MediatorService) { }
 
   ngOnInit(): void {
   }
